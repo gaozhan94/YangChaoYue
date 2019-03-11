8b29bd8dc857d44dfda2d70a582e54b4d7907459
zhan_G <gaozhan94@foxmail.com>
2019-03-12 00:26:12 +0800

    demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,39 @@ const app = getApp()
 
 Page({
   data: {
-    
+    tabBar: 48,
+    shareBtn: true,
   },
   onLoad: function () {
-    
-  }
+    let that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        let tabBar = 48;
+        if (res.system.includes('iOS')) {
+          tabBar = 44;
+        }
+        that.setData({
+          tabBar: tabBar,
+          systemInfo: res,
+        })
+      }
+    })
+  },
+  onShow:function(){
+    let that = this;
+    that.setData({
+      shareBtn: true
+    })
+  },
+  onShareAppMessage:function(){
+    let that = this;
+    that.setData({
+      shareBtn:false
+    })
+    return {
+      title: '抓住那个胖子',
+      path: '/pages/index/index',
+    }
+  },
+
 })
